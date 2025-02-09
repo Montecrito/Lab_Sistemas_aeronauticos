ecd15efa01fec88300ddb1fd07c5ddc8e04f98b7
Montecrito <asamu2003@gmail.com>
2025-02-09 15:46:44 -0400

    Update NRF24_receiver_7channels.ino

--- NRF24_receiver_7channels.ino
+++ NRF24_receiver_7channels.ino
@@ -13,7 +13,7 @@ struct Received_data {
   byte ch2;
   byte ch3;
   byte ch4;
-  byte ch5;
+//  byte ch5;
   //byte ch6;
   //byte ch7;
 };
@@ -24,7 +24,7 @@ Servo channel_1;
 Servo channel_2;
 Servo channel_3;
 Servo channel_4;
-Servo channel_5;
+//Servo channel_5;
 //Servo channel_6;
 //Servo channel_7;
 
@@ -32,7 +32,7 @@ int ch1_value = 0;
 int ch2_value = 0;
 int ch3_value = 0;
 int ch4_value = 0;
-int ch5_value = 0;
+//int ch5_value = 0;
 //int ch6_value = 0;
 //int ch7_value = 0;
 
@@ -44,7 +44,7 @@ void reset_the_Data()
   received_data.ch2 = 127;
   received_data.ch3 = 127;
   received_data.ch4 = 127;
-  received_data.ch5 = 0;
+ // received_data.ch5 = 0;
   //received_data.ch6 = 0;
   //received_data.ch7 = 0;
 }
@@ -114,7 +114,7 @@ void loop()
   ch2_value = map(received_data.ch2,0,255,1000,2000);
   ch3_value = map(received_data.ch3,0,255,1000,2000);
   ch4_value = map(received_data.ch4,0,255,1000,2000);
-  ch5_value = map(received_data.ch5,0,1,1000,2000);
+ // ch5_value = map(received_data.ch5,0,1,1000,2000);
   //ch6_value = map(received_data.ch6,0,1,1000,2000);
   //ch7_value = map(received_data.ch7,0,255,1000,2000);
 
@@ -123,7 +123,7 @@ void loop()
   channel_2.writeMicroseconds(ch2_value);  
   channel_3.writeMicroseconds(ch3_value);  
   channel_4.writeMicroseconds(ch4_value);  
-  channel_5.writeMicroseconds(ch5_value);  
+ // channel_5.writeMicroseconds(ch5_value);  
   //channel_6.writeMicroseconds(ch6_value);  
   //channel_7.writeMicroseconds(ch7_value);  
   
